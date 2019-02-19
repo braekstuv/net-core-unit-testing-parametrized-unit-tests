8ab56e14bda92964038c83c48c25571c61febbde
braekstuv <florianpirard@gmail.com>
2019-02-20 00:44:11 +0100

    refactored tests

--- 2.ClassData/StudentApp.Domain.Test/ClassOfStudents/SortingStudents/HappyPath.cs
+++ 2.ClassData/StudentApp.Domain.Test/ClassOfStudents/SortingStudents/HappyPath.cs
@@ -9,7 +9,8 @@ namespace StudentApp.Domain.Test
 {
     public class HappyPath{
         [Theory]
-        [ClassData(typeof(UnorderedListTestData))]
+        [ClassData(typeof(RandomOrderStudentListData))]
+        [ClassData(typeof(ReverseOrderStudentListData))]
         public void SortingStudents_GivenUnorderedList_ShouldReturnListInExpectedOrder(IEnumerable<Student> unordered, IEnumerable<Student> ordered){
             //Arrange
             var classOfStudents = new ClassOfStudents(0);
@@ -22,71 +23,63 @@ namespace StudentApp.Domain.Test
         }
 
         [Theory]
-        [ClassData(typeof(OrderedStudentsTestData))]
-        public void SortingStudents_GivenUnorderedListAndStudent_StudentShouldBeAtExpectedPosition(Student firstStudent, Student lastStudent, string because){
+        [ClassData(typeof(CompareStudentToSucessorsData))]
+        public void SortingStudents_GivenUnorderedListAndStudent_StudentShouldBeAtExpectedPosition(Student firstStudent, Student secondStudent, string because){
             //Arrange
             var classOfStudents = new ClassOfStudents(0);
-
+           
             //Act
-            classOfStudents.AddStudents(new List<Student>{lastStudent, firstStudent});
+            classOfStudents.AddStudents(new List<Student>{secondStudent, firstStudent});
 
             //Assert
-            classOfStudents.Students.Should().HaveElementAt(0, firstStudent, because);
+            classOfStudents.Students.ToList()[0].Should().Be(firstStudent, because);
         }
     }
 
-    public class OrderedStudentsTestData : IEnumerable<object[]>
+    public class CompareStudentToSucessorsData : IEnumerable<object[]>
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-            yield return new object[] {TestStudents.HighestGradeCount, TestStudents.LastStudent, $"the student that is expected to be first has the highest grade count" };
-            yield return new object[] {TestStudents.HighestGradeAverage, TestStudents.LastStudent, $"the student that is expected to be first has the highest grade average" };
-            yield return new object[] {TestStudents.OldestStudent, TestStudents.LastStudent, $"the student that is expected to be first is the oldest"};
-            yield return new object[] {TestStudents.AlphabeticalFirstStudent, TestStudents.LastStudent, $"the student that is expected to be first is alphabetically first by first and last name"};
+            var students = TestStudents.OrderedStudentsWithOrderReasonList.ToList();
+            for(int i = 0; i < students.Count-1; i++){
+                var firstStudent = students[i];
+                foreach (var secondStudent in students.Skip(i+1))
+                {
+                    yield return new object[] { firstStudent.Student, secondStudent.Student, firstStudent.OrderReason };
+                }
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 
-    public class UnorderedListTestData : IEnumerable<object[]>
+    public class RandomOrderStudentListData : IEnumerable<object[]>
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-            yield return new[] {TestStudents.ReverseOrderedStudentList, TestStudents.OrderedStudentList};
-            yield return new[] {TestStudents.ShuffledOrderedStudentList, TestStudents.OrderedStudentList};
+            var orderedStudents = TestStudents.GetOrderedStudentsList();
+            var shuffleStudents = TestStudents.GetOrderedStudentsList();
+            do{
+                shuffleStudents.Shuffle();
+            }while(shuffleStudents.All(x => shuffleStudents.IndexOf(x) == orderedStudents.IndexOf(x)));
+            yield return new object[] { shuffleStudents, orderedStudents };
         }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
 
+    public class ReverseOrderStudentListData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            var orderedStudents = TestStudents.GetOrderedStudentsList();
+            var reverseStudents = TestStudents.GetOrderedStudentsList();
+            reverseStudents.Reverse();
+            yield return new object[] { reverseStudents, orderedStudents };
+        }
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 
     public static class TestStudents{
-        public static IReadOnlyCollection<Student> OrderedStudentList =>
-            new List<Student>(){
-                HighestGradeCount,
-                HighestGradeAverage,
-                OldestStudent,
-                AlphabeticalFirstStudent,
-                LastStudent
-            };
-
-        public static IReadOnlyCollection<Student> ReverseOrderedStudentList 
-        {
-            get{
-                var students = OrderedStudentList.ToList();
-                students.Reverse();
-                return students;
-            }
-        }
-
-        public static IReadOnlyCollection<Student> ShuffledOrderedStudentList 
-        {
-            get{
-                var students = OrderedStudentList.ToList();
-                students.Shuffle();
-                return students;
-            }
-        }
-
         public static readonly Student HighestGradeCount = new Student{
             Grades = new List<Grade>(){
                 new Grade(new Fraction(1,1),1),
@@ -94,21 +87,18 @@ namespace StudentApp.Domain.Test
             },
             DateOfBirth = new DateTime(1995,02,05)
         };
-
         public static readonly Student HighestGradeAverage = new Student{
             Grades = new List<Grade>(){
                 new Grade(new Fraction(1,1),1),
             },
             DateOfBirth = new DateTime(1995,02,05)
         };
-
         public static readonly Student OldestStudent = new Student{
             Grades = new List<Grade>(){
                 new Grade(new Fraction(1,2),1),
             },
             DateOfBirth = new DateTime(1990,02,10)
         };
-
         public static readonly Student AlphabeticalFirstStudent = new Student{
             FirstName = "AAAA",
             LastName = "AAAAA",
@@ -117,7 +107,6 @@ namespace StudentApp.Domain.Test
             },
             DateOfBirth = new DateTime(1995,02,05)
         };
-
         public static readonly Student LastStudent = new Student{
             FirstName = "ZZZZZZ",
             LastName = "ZZZZZZZ",
@@ -127,20 +116,32 @@ namespace StudentApp.Domain.Test
             DateOfBirth = new DateTime(1995,02,05)
         };
 
+        public static readonly IReadOnlyCollection<(Student Student, string OrderReason)> OrderedStudentsWithOrderReasonList =
+            new List<(Student student, string orderReason)>(){
+                (HighestGradeCount, "this student has the highest grade count"),
+                (HighestGradeAverage, "this student has the highest grade average"),
+                (OldestStudent, "this student is the oldest"),
+                (AlphabeticalFirstStudent, "this student is alphabetically first"),
+                (LastStudent, "this student should be last")
+            };
+
+        public static List<Student> GetOrderedStudentsList(){
+           return OrderedStudentsWithOrderReasonList.Select(x => x.Student).ToList();
+        }
     }
     public static class ShuffleListExtension{
         private static Random rng = new Random();  
 
         public static void Shuffle<T>(this IList<T> list)  
         {  
-            int n = list.Count;  
+            int n = list.Count;
             while (n > 1) {  
                 n--;  
                 int k = rng.Next(n + 1);  
                 T value = list[k];  
                 list[k] = list[n];  
                 list[n] = value;  
-            }  
+            } 
         }
     }
     
